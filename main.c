ad000e16ad2b9b32e3a5ab0e74ccf13f0d460c98
lystyp <lystyp87015@gamil.com>
2020-04-26 18:45:28 +0800

    3rd commit

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include<time.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+// Helololol
 int main(int argc, char** argv) {
 	printf("Hello world!!!");
-	
+	print("");
     return 0;
 }
